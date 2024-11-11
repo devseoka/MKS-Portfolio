67f8974f2e1b8471899506082a6d39f475a7ee08
Moshele Seoka <seokamk@yahoo.com>
2024-11-11 10:00:56 +0200

    Removed Tailwind And SCSS Files

--- mks-portfolio-ui/src/app/app.module.ts
+++ mks-portfolio-ui/src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgOptimizedImage } from '@angular/common'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from '@components/header/header-component';
-import { HomeComponent } from '@components/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgOptimizedImage
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
