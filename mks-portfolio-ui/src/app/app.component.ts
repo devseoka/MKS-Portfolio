40cc82770a4d08daa0800d5c64408d9a97cd988f
Moshele Seoka <seokamk@yahoo.com>
2025-01-18 21:55:47 +0200

    Refactor: Move CV download functionality to a dedicated modal component

--- mks-portfolio-ui/src/app/app.component.ts
+++ mks-portfolio-ui/src/app/app.component.ts
@@ -11,46 +11,9 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  form: FormGroup
   isOpen:boolean = false
-
-  constructor(private cvService: CvGuardService, private fb: FormBuilder) {
-    this.form = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]]
-    })
-  }
   ngOnInit(): void {
     console.log(`The status of the model is `, this.isOpen)
-  }
-  onSubmit() {
-    if (this.form.invalid) {
-      return;
-    }
-    const request = this.form.value
-    this.cvService.download(request).pipe(first()).subscribe({
-      next: (blob) => {
-        const url = window.URL.createObjectURL(blob);
-        const anchor = document.createElement('a');
-        anchor.href = url;
-        const year = new Date().getFullYear();
-        anchor.download = `${environment.name}-${year}-CV.pdf`;
-        anchor.click();
-        window.URL.revokeObjectURL(url);
-        this.close()
-
-      },
-      complete: () => {
-      },
-      error: (e: HttpErrorResponse) => {
-        if (Array.isArray(e.error.errors)) {
-          const errors = e.error.errors;
-        }
-      }
-    })
-  }
-  close() {
-    this.isOpen = !this.isOpen
   }
   onOpen(status: boolean) {
     this.isOpen = status
--- mks-portfolio-ui/src/app/shared/modals/download-cv/download-cv.component.ts
+++ mks-portfolio-ui/src/app/shared/modals/download-cv/download-cv.component.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse } from "@angular/common/http";
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import CvGuardService from "@services/cv-guard.service";
 import { first } from "rxjs";
@@ -10,7 +10,53 @@ import { environment } from "src/environments/environment";
     templateUrl: './download-cv.component.html',
     styleUrls: ['./download.cv.component.scss']
 })
-export class DownloadCvComponent {
+export class DownloadCvComponent implements OnChanges {
+    @Input() isOpen: boolean = false
+     form: FormGroup
     
-    
+      constructor(private cvService: CvGuardService, private fb: FormBuilder) {
+        this.form = this.fb.group({
+          name: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email]]
+        })
+      }
+    ngOnChanges(changes: SimpleChanges): void {
+        const statusObj = changes['isOpen'].currentValue
+        if (statusObj) {
+            console.log(`the status for opening model is =>`, statusObj)
+            this.isOpen = statusObj as boolean
+        }
+    }
+
+    onSubmit() {
+        if (this.form.invalid) {
+            return;
+        }
+        const request = this.form.value
+        this.cvService.download(request).pipe(first()).subscribe({
+            next: (blob) => {
+                const url = window.URL.createObjectURL(blob);
+                const anchor = document.createElement('a');
+                anchor.href = url;
+                const year = new Date().getFullYear();
+                anchor.download = `${environment.name}-${year}-CV.pdf`;
+                anchor.click();
+                window.URL.revokeObjectURL(url);
+                this.close()
+
+            },
+            complete: () => {
+            },
+            error: (e: HttpErrorResponse) => {
+                if (Array.isArray(e.error.errors)) {
+                    const errors = e.error.errors;
+                }
+            }
+        })
+    }
+    close() {
+        this.isOpen = !this.isOpen
+    }
+
+
 }
