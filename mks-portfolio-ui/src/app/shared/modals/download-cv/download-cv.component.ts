848b93e23103070e5d9b57298ca0b5d24c1bc307
Moshele Seoka <seokamk@yahoo.com>
2025-02-01 12:27:46 +0200

    Refactor: Remove unused import of HttpErrorResponse in download-cv component

--- mks-portfolio-ui/src/app/shared/modals/download-cv/download-cv.component.ts
+++ mks-portfolio-ui/src/app/shared/modals/download-cv/download-cv.component.ts
@@ -1,4 +1,3 @@
-import { HttpErrorResponse } from "@angular/common/http";
 import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import CvGuardService from "@services/cv-guard.service";
