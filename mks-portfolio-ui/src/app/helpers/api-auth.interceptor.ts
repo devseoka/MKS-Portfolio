e360306da1252f6863a5fde196b89b532b7730ed
Moshele Seoka <seokamk@yahoo.com>
2025-02-01 23:40:49 +0200

    Refactor: Update auth interceptor to use HttpInterceptorFn and configure HTTP client in app module

--- mks-portfolio-ui/src/app/helpers/api-auth.interceptor.ts
+++ mks-portfolio-ui/src/app/helpers/api-auth.interceptor.ts
@@ -1,24 +1,14 @@
-import { Injectable } from '@angular/core';
-import {
-  HttpEvent,
-  HttpInterceptor,
-  HttpHandler,
-  HttpRequest,
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const clonedRequest = req.clone({
-      setHeaders: {
-        'X-API-Key': `${environment.key}`,
-        'Content-Type': 'application/json',
-        'accept': '*/*',
-      },
-    });
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const clonedRequest = req.clone({
+    setHeaders: {
+      'X-API-Key': `${environment.key}`,
+      'Content-Type': 'application/json',
+      'Accept': '*/*',
+    },
+  });
 
-    return next.handle(clonedRequest);
-  }
+  return next(clonedRequest);
 }
