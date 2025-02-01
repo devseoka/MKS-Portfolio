524f0d5447f087526d1ebcdc9963ee659bc04b18
Moshele Seoka <seokamk@yahoo.com>
2025-02-01 10:50:21 +0200

    Refactor: Replace animation triggers with a unified fadeInOut animation across multiple components

--- mks-portfolio-ui/src/app/components/about/about.component.ts
+++ mks-portfolio-ui/src/app/components/about/about.component.ts
@@ -1,25 +1,21 @@
 import { animate, style, transition, trigger } from "@angular/animations";
 import { Component } from "@angular/core";
-import { fadeInDownAnimation, fadeOutUpAnimation } from "angular-animations";
 
 @Component({
-    selector: 'app-about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.scss'],
-    animations:  [
-        trigger('enter', [
-          transition(':enter', [
-            style({ opacity: 0, transform: 'translateY(-20px)' }),
-            animate('500ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
-          ])
-        ]),
-        trigger('leave', [
-          transition(':leave', [
-            style({ opacity: 1, transform: 'translateY(0)' }),
-            animate('500ms ease-in', style({ opacity: 0, transform: 'translateY(20px)' }))
-          ])
-        ])
-      ]
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(50px)' }),
+        animate('500ms ease-in', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-out', style({ opacity: 0, transform: 'translateY(50px)' }))
+      ])
+    ])
+  ]
 })
 export class AboutComponent {
     
--- mks-portfolio-ui/src/app/components/experience/skills.component.ts
+++ mks-portfolio-ui/src/app/components/experience/skills.component.ts
@@ -1,13 +1,20 @@
+import { animate, style, transition, trigger } from "@angular/animations";
 import { Component } from "@angular/core";
-import { fadeInDownAnimation, fadeOutUpAnimation } from "angular-animations";
-
 @Component({
     selector: 'app-skills',
     templateUrl: './skills.component.html',
     styleUrls: ['./skills.component.scss'],
     animations: [
-        fadeInDownAnimation({ anchor: 'enter', duration: 1000, delay: 100, translate: '30px' }),
-        fadeOutUpAnimation({ anchor: 'leave', duration: 500, delay: 200, translate: '40px' })]
+        trigger('fadeInOut', [
+          transition(':enter', [
+            style({ opacity: 0, transform: 'translateY(50px)' }),
+            animate('500ms ease-in', style({ opacity: 1, transform: 'translateY(0)' }))
+          ]),
+          transition(':leave', [
+            animate('500ms ease-out', style({ opacity: 0, transform: 'translateY(50px)' }))
+          ])
+        ])
+      ]
 })
 export class skillsComponent {
     
