809276619c02e72e89404c12ff4123b98ff5296e
Moshele Seoka <seokamk@yahoo.com>
2025-02-01 11:41:54 +0200

    Refactor: Extend fadeInOut animation duration for smoother transitions in header and skills components

--- mks-portfolio-ui/src/app/components/experience/skills.component.ts
+++ mks-portfolio-ui/src/app/components/experience/skills.component.ts
@@ -8,10 +8,10 @@ import { Component } from "@angular/core";
         trigger('fadeInOut', [
           transition(':enter', [
             style({ opacity: 0, transform: 'translateY(50px)' }),
-            animate('500ms ease-in', style({ opacity: 1, transform: 'translateY(0)' }))
+            animate('1000ms ease-in-out', style({ opacity: 1, transform: 'translateY(0)' }))
           ]),
           transition(':leave', [
-            animate('500ms ease-out', style({ opacity: 0, transform: 'translateY(50px)' }))
+            animate('1000ms ease-in-out', style({ opacity: 0, transform: 'translateY(50px)' }))
           ])
         ])
       ]
--- mks-portfolio-ui/src/app/components/header/header.component.ts
+++ mks-portfolio-ui/src/app/components/header/header.component.ts
@@ -10,10 +10,10 @@ import { bounceOutDownOnLeaveAnimation, fadeInDownAnimation, fadeOutUpAnimation 
         trigger('fadeInOut', [
           transition(':enter', [
             style({ opacity: 0, transform: 'translateY(50px)' }),
-            animate('500ms ease-in', style({ opacity: 1, transform: 'translateY(0)' }))
+            animate('1000ms ease-in-out', style({ opacity: 1, transform: 'translateY(0)' }))
           ]),
           transition(':leave', [
-            animate('500ms ease-out', style({ opacity: 0, transform: 'translateY(50px)' }))
+            animate('1000ms ease-in-out', style({ opacity: 0, transform: 'translateY(50px)' }))
           ])
         ])
       ]
