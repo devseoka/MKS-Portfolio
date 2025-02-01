3bfbb3444358370d14a8cf1e85bb9a5f846a2080
Moshele Seoka <seokamk@yahoo.com>
2025-02-01 10:38:26 +0200

    Fix: Update API endpoint to use ngrok for development

--- mks-portfolio-ui/src/environments/environment.ts
+++ mks-portfolio-ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    api: 'https://localhost:7003/api',
+    api: 'https://fbbd-41-113-246-73.ngrok-free.app/api',
     key: 'MKS-1737202050-7ZM42PEH-RKA18FGM',
     name: 'Moshele Seoka-NET Full Stack Developer'
 };
