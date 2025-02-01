aec4771216e21c3a4e837f1dbd77c4b9b687404f
Moshele Seoka <seokamk@yahoo.com>
2025-02-01 12:29:17 +0200

    Refactor: Remove unused animation imports in header component

--- mks-portfolio-ui/src/app/components/header/header.component.ts
+++ mks-portfolio-ui/src/app/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { animate, style, transition, trigger } from "@angular/animations";
 import { Component } from "@angular/core";
-import { bounceOutDownOnLeaveAnimation, fadeInDownAnimation, fadeOutUpAnimation } from "angular-animations";
 
 @Component({
     selector: 'app-introduction',
