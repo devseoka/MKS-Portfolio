5feb1adc73525ffdaec640c23fdfafffac76b0d2
Moshele Seoka <seokamk@yahoo.com>
2025-02-01 13:07:06 +0200

    Refactor: Update CI workflow to streamline build and deployment process

--- mks-portfolio-ui/src/environments/environment.ts
+++ mks-portfolio-ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    api: 'https://fbbd-41-113-246-73.ngrok-free.app/api',
+    api: 'https://5c6c-41-113-246-73.ngrok-free.app/api',
     key: 'MKS-1737202050-7ZM42PEH-RKA18FGM',
     name: 'Moshele Seoka-NET Full Stack Developer'
 };
