c929d72ee5f667ecd37bb3b71c07edd710ef97ad
Moshele Seoka <seokamk@yahoo.com>
2025-02-02 09:54:00 +0200

    Refactor: Update API endpoint to new Azure hosting URL

--- mks-portfolio-ui/src/environments/environment.ts
+++ mks-portfolio-ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api: 'https://fbbd-41-113-246-73.ngrok-free.app/api',
+  api: 'https://api-sds-cv-guard-defrb2gzhahfckgr.ukwest-01.azurewebsites.net/api',
   key: 'MKS-1737202050-7ZM42PEH-RKA18FGM',
   name: 'Moshele Seoka-NET Full Stack Developer',
 };
