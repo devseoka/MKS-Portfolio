72221ffa0c2c6baac3a8364141506d521745b826
Moshele Seoka <seokamk@yahoo.com>
2025-02-08 20:23:30 +0200

    Fix: Added a loading screen component

--- mks-portfolio-ui/src/app/app.component.ts
+++ mks-portfolio-ui/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Health } from '@models/health.response';
 import { first } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -12,25 +13,25 @@ import { environment } from 'src/environments/environment';
 export class AppComponent implements OnInit {
   constructor(private router: Router, private http: HttpClient){}
   isOpen = false;
-  endpoint = `${environment.api}/cv/download`
-  isRunning = true
+  endpoint = `${environment.api}/api/health`
+  isRunning = false
+  isHealthCheckCompleted = false
   ngOnInit(): void {
     this.checkServiceStatus()
   }
   onOpen(status: boolean) {
     this.isOpen = status;
-    
   }
   checkServiceStatus() {
-  const body =  { email: 'support@seokamoshele.digital', name: 'Moshele Seoka'}
-   this.http.post(this.endpoint, body).pipe(first()).subscribe({
-    complete: () => {
-      this.isRunning
+   this.http.get<Health>(this.endpoint).pipe(first()).subscribe({
+    next: (response) => {
+     this.isRunning = response.status == 'Healthy'
+     this.isHealthCheckCompleted = true
     },
-     error: () => {
+    error: () => {
        this.isRunning = false
-        this.router.navigate(['/503'])
-       
+       this.isHealthCheckCompleted = true
+       this.router.navigate(['/503']) 
      }
    })
   }
---
+++ mks-portfolio-ui/src/app/shared/loader/loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-loader',
+    styleUrls: ['./loader.component.scss'],
+    templateUrl: './loader.component.html'
+})
+export class LoaderComponent {
+
+}
